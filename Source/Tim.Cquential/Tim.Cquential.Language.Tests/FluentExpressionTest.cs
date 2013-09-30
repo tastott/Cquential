8197dbf9e00ad5773920d0787c2743572132e4c6
Tim Stott <ta.stott@googlemail.com>
2013-09-30 19:36:07 +0100

    Moved fluent api stubs into Language project

--- Source/Tim.Cquential/Tim.Cquential.Language.Tests/FluentExpressionTest.cs
+++ Source/Tim.Cquential/Tim.Cquential.Language.Tests/FluentExpressionTest.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Tim.Cquential.Core.Expressions.Fluent
+namespace Tim.Cquential.Language.Fluent
 {
     [TestClass]
     public class FluentExpressionTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void FluentExpressionTest()
         {
             FluentExpression.Make<DateTime>()
                 .StaticItem(0, d => d.Day)
--- Source/Tim.Cquential/Tim.Cquential.Language/Fluent/FluentOperatorBuilder.cs
+++ Source/Tim.Cquential/Tim.Cquential.Language/Fluent/FluentOperatorBuilder.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tim.Cquential.Core;
 
-namespace Tim.Cquential.Core.Expressions.Fluent
+namespace Tim.Cquential.Language.Fluent
 {
     public class FluentOperatorBuilder<T>
     {
