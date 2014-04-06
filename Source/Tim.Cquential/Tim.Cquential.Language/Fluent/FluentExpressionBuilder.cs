6faed7e74804bc65f1650bd0ce5b61ef4b0f004c
Tim Stott <ta.stott@googlemail.com>
2014-04-06 15:32:45 +0100

    Bit of tidying up on Fluent expression builder but implementing this will require a bit more thought!

---
+++ Source/Tim.Cquential/Tim.Cquential.Language/Fluent/FluentExpressionBuilder.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tim.Cquential.Language.Fluent
+{
+    using Core;
+    using Core.Aggregation;
+
+    public interface IFluentOperatorBuilder<T>
+    {
+        IFluentOperandBuilder<T> IsGreaterThan();
+        IFluentOperandBuilder<T> And();
+        IFluentOperandBuilder<T> Times();
+
+        IQuery<T> ToQuery();
+    }
+
+
+
+    public interface IFluentOperandBuilder<T>
+    {
+        IFluentOperatorBuilder<T> StaticItem(int itemIndex, System.Linq.Expressions.Expression<Func<T, double>> memberExpression);
+        IFluentOperatorBuilder<T> Constant(double value);
+        IFluentOperatorBuilder<T> Max(System.Linq.Expressions.Expression<Func<T, double>> memberExpression);
+        IFluentOperatorBuilder<T> Min(System.Linq.Expressions.Expression<Func<T, double>> memberExpression);
+    }
+
+    public static class FluentExpression
+    {
+        public static FluentExpressionBuilder<T> Make<T>()
+        {
+            return new FluentExpressionBuilder<T>();
+        }
+    }
+
+    public class FluentExpressionBuilder<T> : IFluentOperandBuilder<T>, IFluentOperatorBuilder<T>
+    {
+        private AggregatingExpressionFactory<T> _expFactory;
+
+        public FluentExpressionBuilder()
+        {
+            _expFactory = new AggregatingExpressionFactory<T>();
+        }
+
+        public IQuery<T> ToQuery()
+        {
+            return null;
+        }
+
+        public IFluentOperandBuilder<T> IsGreaterThan()
+        {
+            return this;
+        }
+
+        public IFluentOperandBuilder<T> And()
+        {
+            return this;
+        }
+
+        public IFluentOperandBuilder<T> Times()
+        {
+            return this;
+        }
+
+        public IFluentOperatorBuilder<T> StaticItem(int itemIndex, System.Linq.Expressions.Expression<Func<T, double>> memberExpression)
+        {
+            return this;
+        }
+
+        public IFluentOperatorBuilder<T> Constant(double value)
+        {
+            return this;
+        }
+
+        public IFluentOperatorBuilder<T> Max(System.Linq.Expressions.Expression<Func<T, double>> memberExpression)
+        {
+            return this;
+        }
+
+        public IFluentOperatorBuilder<T> Min(System.Linq.Expressions.Expression<Func<T, double>> memberExpression)
+        {
+            return this;
+        }
+    }
+}
